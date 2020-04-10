df4dc21aea62843c6d2d3a4fec34c142e242fa97
ferakp <akpinarferidun92@icloud.com>
2020-04-10 22:16:06 +0300

    Fix responsive issue

--- cs-box-container/cs-box-container.js
+++ cs-box-container/cs-box-container.js
@@ -117,7 +117,7 @@ class CustomBoxContainer extends HTMLElement {
     initializeStyle = (style) => {
 
         style.innerText = `
-                            :host(*) {
+                            * {
                                 font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
                                 box-sizing: border-box;
                             }
